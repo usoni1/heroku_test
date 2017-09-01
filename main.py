3b58e82707cae02bcae23d132b70a04e64aff910
usoni1 <usoni1@asu.edu>
2017-09-01 15:50:12 -0700

    templates added

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from pymongo import MongoClient
 
+MONGO_URL = os.environ.get('MONGOHQ_URL')
+client = MongoClient(MONGO_URL)
 app = Flask(__name__)
+db = client.app76102553
+collection = db.shoutouts
 
-@app.route("/")
-def hello():
-    return "Hello world!"
+@app.route("/", methods=['GET'])
+def index():
+    shouts = collection.find()
+    return render_template('index.html', shouts = shouts)
+
+@app.route("/post", methods=['POST'])
+def post():
+    shout = {"name" : request.form['name'], "message" : request.form['message']}
+    shout_id = collection.insert(shout)
+    return redirect('/')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
