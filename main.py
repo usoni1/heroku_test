cbfaa4e37b19b14cd69306e2055532cf5773550c
usoni1 <usoni1@asu.edu>
2017-09-05 14:19:58 -0700

    heroku local additions

--- main.py
+++ main.py
@@ -30,5 +30,6 @@ def starred():
     return redirect('/')
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    # app.run(host='0.0.0.0', port=port)
+    app.run()
